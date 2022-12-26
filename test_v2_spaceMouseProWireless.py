e18d077be5d95d8acffa1a257c06b4486798983c
Urban Willi <urban.willi@stud.fhgr.ch>
2022-12-27 00:33:07 +0100

    before changes to sys.modules stub

--- test_v2_spaceMouseProWireless.py
+++ test_v2_spaceMouseProWireless.py
@@ -7,7 +7,7 @@ import pytest
 import sys
 
 import usb.core
-from stub_usb import Device as Stub_Device
+from stub_usb.stub_usb import Device as Stub_Device
 
 import spaceMouseProWireless as Sm
 
@@ -24,11 +24,24 @@ class TestUsbConnectionEstablishment:
         )
 
     @staticmethod
+    @pytest.fixture(autouse=True, scope='function')
+    def mock_usb_dispose(mocker):
+        mocker.patch(
+            'usb.util.dispose_resources',
+            return_value=None
+        )
+
+    @staticmethod
     @pytest.fixture(scope='function')
     def mock_ct(mock_usb_core_find):
         ct = Sm.SpaceMouseProWireless()
 
         return ct
+
+    @staticmethod
+    @pytest.fixture(scope='class')
+    def mock_usb(class_mocker):
+        class_mocker.patch.object() # USB-package mocked as a whole
 
     # ===================== Tests ==========================================
     @staticmethod
